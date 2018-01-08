119bce74dd8292b126f67cb85a4cb77595380085
Your Name <you@example.com>
2018-01-08 16:48:01 +0100

    lundi

--- Android_studio/app/src/main/java/fr/aston/helloword/MainActivity.java
+++ Android_studio/app/src/main/java/fr/aston/helloword/MainActivity.java
@@ -2,12 +2,17 @@ package fr.aston.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //afficher le template associé
         setContentView(R.layout.activity_main);
+
+        Log.e("toto", "message");
     }
 }
